305e49ff141ba9b925d852b9e7809818d2475348
c.diddens <c.diddens@utwente.nl>
2026-07-07 07:08:43 +0200

    Changed mail

--- src/bifurcation.hpp
+++ src/bifurcation.hpp
@@ -15,7 +15,7 @@ GNU General Public License for more details.
 You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>. 
 
-The authors may be contacted at c.diddens@utwente.nl and d.rocha@utwente.nl
+The main author may be contacted at c.diddens@utwente.nl
 
 ================================================================================*/
 
